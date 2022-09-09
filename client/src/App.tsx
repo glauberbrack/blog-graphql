e6deb096c3de44a7025f9c57639cc1b0a35c5dc1
glauberbrack <glauber@brack.com.br>
2022-09-08 23:23:49 -0300

    feat(client): project setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from "@apollo/client";
 
-function App() {
+import request from "./api/request";
+import Home from "./screens/Home";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={request}>
+      <Home />
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
